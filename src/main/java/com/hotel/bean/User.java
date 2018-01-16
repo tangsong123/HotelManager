fd1ba817b656c286256dc726b55dfa7dbd11c534
tangsong123 <artisan_song@163.com>
2018-01-16 17:44:59 +0800

    新增了 bean dao service mapper

---
+++ src/main/java/com/hotel/bean/User.java
@@ -0,0 +1,43 @@
+package com.hotel.bean;
+
+public class User {
+    private Long id;
+
+    private String userName;
+
+    private Integer userAge;
+
+    private String userHobby;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public Integer getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(Integer userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getUserHobby() {
+        return userHobby;
+    }
+
+    public void setUserHobby(String userHobby) {
+        this.userHobby = userHobby == null ? null : userHobby.trim();
+    }
+}
