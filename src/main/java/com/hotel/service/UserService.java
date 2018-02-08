232bbb1e9481a7f43ceb7f94b754bf97ced189ab
tangsong123 <artisan_song@163.com>
2018-02-08 21:42:53 +0800

    小微调

--- src/main/java/com/hotel/service/UserService.java
+++ src/main/java/com/hotel/service/UserService.java
@@ -5,7 +5,6 @@ import com.hotel.dao.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service("userService")
@@ -14,9 +13,6 @@ public class UserService{
     UserMapper userMapper;
 
     public List<User> getAll() {
-//        List<User> users = new ArrayList<User>();
-
-
         return userMapper.userList();
     }
 }
